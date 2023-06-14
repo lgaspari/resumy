555fc3a6d757bbf28ea4e2676d8acbab0398175b
Leandro Gaspari <leandro.ezequiel.gaspari@gmail.com>
2023-06-14 17:22:38 -0300

    chore: remove unused theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,6 @@ module.exports = {
   plugins: [],
   theme: {
     extend: {
-      colors: {
-        shirt: {
-          100: '#f9fafc',
-          200: '#d8dcea',
-          300: '#b7bed8',
-          400: '#96a0c6',
-          500: '#5666a0',
-          600: '#44517f',
-          700: '#323c5e',
-          800: '#21273c',
-          900: '#0f111b',
-        },
-      },
       fontFamily: {
         notoSans: ['Noto Sans', 'sans-serif'],
       },
